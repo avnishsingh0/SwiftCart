cafbaedcdbdebc1b5a21969c55f14833e64256f8
avnish <avnishbdp@gmail.com>
2023-05-13 09:56:27 +0530

    authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {LoginPage, SignupPage,ActivationPage} from './Routes.js'
+import './App.css'
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
+import Store from './Redux/store.js'
+import { loadUser } from './Redux/Action/user.js'
 const App = () => {
+  useEffect(() => {
+    Store.dispatch(loadUser());
+  }, []);
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/sign-up' element={<SignupPage/>}/>
+        <Route path='/activation/:activation_token' element={<ActivationPage/>}/>
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/Redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./Reducer/user";
+
+const Store = configureStore({
+    reducer: {
+        user: userReducer,
+    },
+  });
+  
+  export default Store;
