dd3a0a8f3ff421b3d461108ecd1845cf67577abf
avnish <avnishbdp@gmail.com>
2023-05-14 09:23:54 +0530

    sponsored

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Categories from "../components/Route/Categories/Categories.jsx"
 import BestDeals from "../components/Route/BestDeals/BestDeals.jsx"
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct.jsx"
 import Events from "../components/Route/Events/Events.jsx"
-
+import Sponsored from "../components/Route/Sponsored.jsx"
 const HomePage = () => {
   return (
     <div>
@@ -15,6 +15,7 @@ const HomePage = () => {
         <BestDeals/>
         <Events/>
         <FeaturedProduct/>
+        <Sponsored/>
 
     </div>
   )
