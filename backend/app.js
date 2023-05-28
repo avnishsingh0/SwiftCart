805575901b16ca2c22f97a0d23acb21920fcc798
avnish <avnishbdp@gmail.com>
2023-05-28 12:50:36 +0530

    payment_stripe

--- backend/app.js
+++ backend/app.js
@@ -28,12 +28,14 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   const product = require("./controller/product");
 const event = require('./controller/event');
 const coupon = require('./controller/coupounCode');
+const payment = require('./controller/payment');
 
   app.use("/api/v2/user",user)
   app.use("/api/v2/shop",shop)
   app.use("/api/v2/product",product)
   app.use("/api/v2/event",event)
   app.use("/api/v2/coupon",coupon)
+  app.use("/api/v2/payment",payment)
 
   const errorHandlerMiddleware = (err, req, res, next) => {
     if (err instanceof ErrorHandler) {
