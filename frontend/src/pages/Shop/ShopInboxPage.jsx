e80695fbaaa7356fce4ce4dec3918120f50568a6
avnish <avnishbdp@gmail.com>
2023-05-29 14:15:09 +0530

    chating_feature

--- frontend/src/pages/Shop/ShopInboxPage.jsx
+++ frontend/src/pages/Shop/ShopInboxPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader'
+import DashboardSideBar from '../../components/Shop/Layout/DashboardSideBar'
+import DashboardMessages from "../../components/Shop/DashboardMessages.jsx";
 
 const ShopInboxPage = () => {
   return (
-    <div>ShopInboxPage</div>
+    <div>
+    <DashboardHeader />
+    <div className="flex items-start justify-between w-full">
+      <div className="w-[80px] 800px:w-[330px]">
+        <DashboardSideBar active={8} />
+      </div>
+       <DashboardMessages />
+    </div>
+  </div>
   )
 }
 
