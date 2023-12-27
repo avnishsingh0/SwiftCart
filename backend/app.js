8a6f10cc0802f4ac5b6595abb28473eda809936a
avnishsingh0 <avnishbdp@gmail.com>
2023-12-27 16:01:10 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://swiftcart-web.vercel.app",
     credentials: true,
   })
 );
