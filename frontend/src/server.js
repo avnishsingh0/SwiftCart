72ab22971449fdfddb23d3d74079337e9e7f33f6
avnishsingh0 <avnishbdp@gmail.com>
2023-12-27 15:59:56 +0530

    updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000";
+export const server = "https://giddy-plum-elephant.cyclic.app/";
+export const backend_url = "https://giddy-plum-elephant.cyclic.app/";
